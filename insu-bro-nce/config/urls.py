3ae97303a4fb23f969358f8742673b74d9e085c8
BPIlyich <bundin.pavel@gmail.com>
2021-07-25 00:52:46 +0400

    Добавлены пользовательские urls и настроен редирект на главную страницу

--- insu-bro-nce/config/urls.py
+++ insu-bro-nce/config/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # FIXME: Временная затычка
-    path('', TemplateView.as_view(template_name='base.html'), name='base'),
-
+    path('', RedirectView.as_view(pattern_name='insurance:product:table'), name='base'),
     path('insurance/', include('insurance.urls')),
+    path('users/', include('users.urls')),
 ]
