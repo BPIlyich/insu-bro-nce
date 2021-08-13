845580601fc56f88b4e7f7f430b47ac3336a1a15
BPIlyich <bundin.pavel@gmail.com>
2021-08-13 20:39:44 +0400

    В админку добавлен вывод счётчика просмотров для страховых продуктов

--- insu-bro-nce/insurance/admin.py
+++ insu-bro-nce/insurance/admin.py
@@ -7,6 +7,10 @@ from .models import (
 )
 
 
+class InsuranceProductAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'page_view_counter')
+
+
 admin.site.register(InsuranceProductCategory)
-admin.site.register(InsuranceProduct)
+admin.site.register(InsuranceProduct, InsuranceProductAdmin)
 admin.site.register(InsuranceProductResponse)
